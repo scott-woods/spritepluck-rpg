1f754bff3ca4173aa7bf1c0dfc170b9a1aaa1ef2
scott-woods <scott_woods44@yahoo.com>
2023-04-08 11:31:25 -0400

    enemy changes

--- modules/characters/enemies/fast_enemy/fast_enemy.gd
+++ modules/characters/enemies/fast_enemy/fast_enemy.gd
@@ -2,6 +2,9 @@ class_name FastEnemy
 extends Enemy
 
 
+@onready var sprite : Sprite2D = $Sprite
+@onready var hurtbox : Hurtbox = $Hurtbox
+@onready var collision : CollisionShape2D = $Collision
 @onready var melee_path : Path2D = $MeleePath
 @onready var melee_path_follow : PathFollow2D = $MeleePath/MeleePathFollow
 @onready var melee : Hitbox = $MeleePath/MeleePathFollow/Melee
